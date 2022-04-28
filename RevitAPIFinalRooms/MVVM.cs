83bf02c0fc4c08fd3cc7f31434d0c573f1aa5a6c
Andrey Salnikov <kvanta13@yandex.ru>
2022-04-29 01:04:35 +0300

    Add project files.

---
+++ RevitAPIFinalRooms/MVVM.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIFinalRooms
+{
+    public class MVVM
+    {
+        public ExternalCommandData _commandData;
+        public DelegateCommand saveCommand { get; }
+
+
+        public MVVM(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            saveCommand = new DelegateCommand(SaveCommand);
+
+        }
+
+        private void SaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            return;
+        }
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ RevitAPIFinalRooms/Main.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIFinalRooms
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            List<Level> levels = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            Transaction ts = new Transaction(doc, "insert rooms");
+            ts.Start();
+            ICollection<ElementId> rooms;
+            foreach (Level level in levels)
+            {
+                rooms = doc.Create.NewRooms2(level);
+            }
+            FilteredElementCollector roomfilter = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Rooms);
+            IList<ElementId> roomsID = roomfilter
+                .ToElementIds() as IList<ElementId>;
+            foreach (ElementId id in roomsID)
+            {
+                Element e = doc.GetElement(id);
+                Room r = e as Room;
+                string lName = r.Level.Name.Substring(6);
+                r.Name = $"{lName}_{r.Number}";
+                doc.Create.NewRoomTag(new LinkElementId(id),
+                                        new UV(0, 0),
+                                        null);
+            }
+            ts.Commit();
+
+            return Result.Succeeded;
+        }
+    }
+}
